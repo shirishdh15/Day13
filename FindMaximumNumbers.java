7fbcf65585ccaf6f077733526daf24a3e2f2b69d
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-02-07 07:58:33 +0530

    Add files via upload

---
+++ FindMaximumNumbers.java
@@ -0,0 +1,10 @@
+package Day13;
+
+public class FindMaximumNumbers {
+    public FindMaximumNumbers(int i, int i1, int i2) {
+
+    }
+
+    public void sort(int[] ints) {
+    }
+}
--- MaximumNumber.java
+++ MaximumNumber.java
@@ -33,4 +33,15 @@ public class MaximumNumber<T extends Comparable<T>> {
 
     private void testMax(T x, T y, T z) {
     }
+
+
+    public static void main(String args[]) {
+        System.out.printf("Maximum of %d, %d and %d is %d\n\n", 3, 4, 5, maximum(3, 4, 5));
+        System.out.printf("Maximum of %.1f, %.1f and %.1f is %.1f\n\n", 6.6, 8.8, 7.7, maximum(6.6,
+                8.8, 7.7));
+        System.out.printf("Maximum of %s, %s and %s is %s\n", "Apple", "Peach", "Banana", maximum(
+                "Apple", "Peach", "Banana"));
+        FindMaximumNumbers findMaximumNumbers = new FindMaximumNumbers(10,20,30);
+        findMaximumNumbers.sort(new int [] {15,2,3});
+    }
 }
