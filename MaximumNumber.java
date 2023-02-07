48d8a729925d40561eebbb9a54e86c61e38ed593
shirishdh15 <121274610+shirishdh15@users.noreply.github.com>
2023-02-07 07:51:06 +0530

    Add files via upload

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -11,5 +11,19 @@ public class MaximumNumber<T extends Comparable<T>> {
 
         return max;
     }
+    public void testMaximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(max) > 0)
+            max = y;
 
+        if (z.compareTo(max) > 0)
+            max = z;
+        System.out.println("Maximum value is :" +max);
+    }
+
+
+    public void sort( int [] sortElements) {
+        Arrays.sort(sortElements);
+        System.out.println(Arrays.toString(sortElements));
+    }
 }
